785705ad5ee087d15422cb8b81ee944dd76bf518
Satyam <nikamsatyam187@gmail.com>
2025-11-26 10:42:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-// Person constructor
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
-// Person prototype method
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
-// Employee constructor
 function Employee(name, age, jobTitle) {
-  Person.call(this, name, age); // inherit properties
+  Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// Inherit Person prototype
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// Employee prototype method
 Employee.prototype.jobGreet = function () {
   console.log(
     `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
